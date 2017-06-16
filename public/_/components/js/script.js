934d103fa9c99a91b777450a83ce5ce4f9d4e355
Denis Bouquet <den.bouquet@gmail.com>
2017-06-16 15:54:22 +0100

    remove transition opacity global and careers, add rule rollover

--- public/_/components/js/script.js
+++ public/_/components/js/script.js
@@ -207,7 +207,13 @@ f=new sa(C,u,C[u],D,f),u in A&&(f.e=A[u]),f.xs0=0,f.plugin=h,d._overwriteProps.p
 		// won't work on windows resize
 		if( $(window).width() > 767 ) {
 			this.firstNav.on('mouseenter', function(e) {
-				$(this).trigger("click");
+
+				if($(this).parents('li').hasClass('is-open')) {
+					// if menu item already open do nothing on rollover
+				}
+				else {
+					$(this).trigger("click");
+				}
 			});
 			// $('header.main').on('mouseout', function(e) {
 				// $('#cbp-tm-menu > li').removeClass('is-open cbp-tm-show cbp-tm-show-below');
